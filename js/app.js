07c33003c1accbcaee188307f1678d8a30181b6a
fataler <fataler69@yandex.ru>
2017-05-21 10:55:40 +0300

    initial commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$('#tech').click(function(){
+	$('#tech').prop('checked')?$('#tech-c').fadeIn():$('#tech-c').fadeOut();
+});
+
+$('#oper').click(function(){
+	$('#oper').prop('checked')?$('#oper-c').fadeIn():$('#oper-c').fadeOut();
+});
+
+$('#sps').click(function(){
+	$('#sps').prop('checked')?$('#sps-c').fadeIn():$('#sps-c').fadeOut();
+});
