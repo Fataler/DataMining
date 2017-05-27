cfeba0997eadeabf5bdbcfdb3403782e249dff94
fataler <fataler69@yandex.ru>
2017-05-27 12:11:33 +0300

    Minor changes

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
-$('#tech').click(function(){
-	$('#tech').prop('checked')?$('#tech-c').fadeIn():$('#tech-c').fadeOut();
+$(".button-collapse").sideNav();
+$('#tech').click(function () {
+	$('#tech').prop('checked') ? $('#tech-c').fadeIn() : $('#tech-c').fadeOut();
+	
 });
 
-$('#oper').click(function(){
-	$('#oper').prop('checked')?$('#oper-c').fadeIn():$('#oper-c').fadeOut();
+$('#oper').click(function () {
+	$('#oper').prop('checked') ? $('#oper-c').fadeIn() : $('#oper-c').fadeOut();
 });
 
-$('#sps').click(function(){
-	$('#sps').prop('checked')?$('#sps-c').fadeIn():$('#sps-c').fadeOut();
+$('#sps').click(function () {
+	$('#sps').prop('checked') ? $('#sps-c').fadeIn() : $('#sps-c').fadeOut();
 });
