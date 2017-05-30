3c200d8a95f1f7b5fe9ad3258c12849c67924f37
fataler <fataler69@yandex.ru>
2017-05-30 15:09:38 +0300

    Minor Fixes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
+
 $(".button-collapse").sideNav();
 $('#tech').click(function () {
 	$('#tech').prop('checked') ? $('#tech-c').fadeIn() : $('#tech-c').fadeOut();
-	
+
 });
 
 $('#oper').click(function () {
